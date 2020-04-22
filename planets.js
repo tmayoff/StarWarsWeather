1e97af912719920fc0651e1d4fdc11d14e093592
Tyler <tyler@tylermayoff.com>
2020-04-22 12:07:32 -0400

    moved planets to seperate folder for easy editing

---
+++ planets.js
@@ -0,0 +1,31 @@
+// Temperature in Kelvin
+
+var planets = [ {
+	name: 'hoth',
+	min_temp: 0,
+	max_temp: 263.15,
+	img: '/images/hoth.jpg',
+	color: 'dark'
+  }, {
+	name: 'jakku',
+	min_temp: 293.15,
+	max_temp: 304.15,
+	img: '/images/jakku.jpg',
+	color: 'dark'
+  }, {
+	name: 'degobah',
+	min_temp: 263.15,
+	max_temp: 283.15,
+	min_humidity: 80,
+	img: '/degobah.jpg',
+	color: 'dark'
+  }, {
+	name: 'endor',
+	min_temp: 263.15,
+	max_temp: 293.15,
+	img: '/images/endor.jpg',
+	color: 'dark'
+  }
+]
+
+module.exports = planets;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var request = require('request');
 var config = require('../config');
+var planets = require('../planets');
 
 var router = express.Router();
 
@@ -40,33 +41,7 @@ function kelvinToCelsius (k) {
 }
 
 function getPlanet (temp, humidity) {
-  let planets = [ {
-      name: 'hoth',
-      min_temp: 0,
-      max_temp: 263.15,
-      img: '/images/hoth.jpg',
-      color: 'dark'
-    }, {
-      name: 'jakku',
-      min_temp: 293.15,
-      max_temp: 304.15,
-      img: '/images/jakku.jpg',
-      color: 'dark'
-    }, {
-      name: 'degobah',
-      min_temp: 263.15,
-      max_temp: 283.15,
-      min_humidity: 80,
-      img: '/degobah.jpg',
-      color: 'dark'
-    }, {
-      name: 'endor',
-      min_temp: 263.15,
-      max_temp: 293.15,
-      img: '/images/endor.jpg',
-      color: 'dark'
-    }
-  ]
+  
 
   let currentPlanet;
   planets.forEach(planet => {
